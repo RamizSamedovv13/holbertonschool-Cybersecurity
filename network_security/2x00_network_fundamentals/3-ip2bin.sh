8f3606152c8bd24eebc5e7123ce5ecc4fe02c675
RamizSəmədovv13 <ramizsmdov11@gmail.com>
2026-05-22 12:54:20 -0400

    Fif2

--- network_security/2x00_network_fundamentals/3-ip2bin.sh
+++ network_security/2x00_network_fundamentals/3-ip2bin.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-echo "$1" | tr '.' '\n' | while read octet
-do
-    printf "%08d" "$(echo "obase=2; $octet" | bc)"
-    [ "$octet" != "$(echo $1 | cut -d. -f4)" ] && echo -n "."
-done
-
-echo
+for o in ${1//./ }; do printf "%08d." "$(echo "obase=2;$o" | bc)"; done | sed 's/\.$//'
